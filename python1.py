288e5a7046d908ea35a9d8ba4b60523740e28003
Tarjeet <tarjeet@tarjeet.com>
2019-06-20 13:55:41 -0700

    python code 1

---
+++ python1.py
@@ -0,0 +1,12 @@
+name1 = "Python"
+name2 = "Ruby"
+name3 = "Perl"
+
+print ("{:>30}".format(name1))
+print ("{:>30}".format(name2))
+print ("{:>30}".format(name3))
+print()
+
+name4 = input("Enter name 4: ")
+print ("{:30}".format(name4))
+
